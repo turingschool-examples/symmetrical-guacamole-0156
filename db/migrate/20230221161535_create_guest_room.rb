71825ec9234d31b6b32a32876d7068a958a652df
Hady Matar <hadyematar@gmail.com>
2023-02-21 09:16:41 -0700

    Relationship setup and tests passing

---
+++ db/migrate/20230221161535_create_guest_room.rb
@@ -0,0 +1,8 @@
+class CreateGuestRoom < ActiveRecord::Migration[5.2]
+  def change
+    create_table :guest_rooms do |t|
+      t.references :guest, foreign_key: true
+      t.references :room, foreign_key: true
+    end
+  end
+end
---
+++ spec/models/guestroom_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe GuestRoom, type: :model do
+  describe 'relationships' do
+    it {should belong_to :guest}
+    it {should belong_to :room}
+  end
+end 
