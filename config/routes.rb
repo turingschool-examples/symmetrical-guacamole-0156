bfb534847b5326834502a34689cd5d1795513e0c
KaraJH <karas.jones@yahoo.com>
2023-02-21 10:01:10 -0700

    feat: Route and controller for guest rooms

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/guests/:guest_id', to: 'guests#show'
+
+  post '/guest_rooms/:guest_id', to: 'guest_rooms#create'
 end
