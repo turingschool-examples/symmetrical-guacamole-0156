1bb5fdc5296c78fd56a749cb519af66fc0c1cc85
Brad Dunlap <braddunlapdpt@gmail.com>
2023-02-21 11:13:45 -0500

    feat: set up relationships and created migrations

---
+++ spec/models/room_guest_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe RoomGuest, type: :model do
+  describe 'relationships' do
+    it {should belong_to :room}
+    it {should belong_to :guest}
+  end
+end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe Room, type: :model do
   describe 'relationships' do
     it {should belong_to :hotel}
+		it {should have_many :guests}
+    it {should have_many :room_guests}
   end
 end
