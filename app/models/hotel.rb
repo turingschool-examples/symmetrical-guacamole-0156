4fddf3fedd93b8e6a439b5d9d3c9b852b4c8c814
KaraJH <karas.jones@yahoo.com>
2023-02-21 10:53:33 -0700

    Extension complete with passing test

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,3 +1,9 @@
 class Hotel < ApplicationRecord
    has_many :rooms
+   has_many :guest_rooms, through: :rooms
+   has_many :guests, through: :guest_rooms
+
+   def guests_stayed 
+      guests.distinct
+   end
 end
