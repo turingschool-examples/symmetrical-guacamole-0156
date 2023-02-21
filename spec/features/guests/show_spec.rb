9d37f855b2781a3adafe99eb9fd6a943a85c10e8
Matt Enyeart <menyeart1@gmail.com>
2023-02-21 10:12:57 -0700

    Refactor form test to account for finding substring in string

--- spec/features/guests/show_spec.rb
+++ spec/features/guests/show_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe 'the guest show page' do
   describe "as a visitor" do
     describe "when I visit a guest's show page" do
       it "it lists all the rooms they've stayed in, cinluding the room's suite, nightly rate and name of the hotel it belongs to" do
-        stanley = Hotel.create(name: "The Stanley", location: "Estes Park")
-        grand_budapest = Hotel.create(name: "The Grand Budapest Hotel", location: "Budapest")
-        pres = Room.create(suite: 'Presidential', rate: 400, hotel: stanley)
-        king = Room.create(suite: 'King', rate: 500, hotel: stanley)
-        poor = Room.create(suite: 'Poor', rate: 50, hotel: grand_budapest)
-        matt = Guest.create(name: 'Matt', nights: 3)
-        guestroom1 = GuestRoom.create(guest: matt, room: pres )
-        guestroom2 = GuestRoom.create(guest: matt, room: king )
+        stanley = Hotel.create!(name: "The Stanley", location: "Estes Park")
+        grand_budapest = Hotel.create!(name: "The Grand Budapest Hotel", location: "Budapest")
+        pres = Room.create!(suite: 'Presidential', rate: 400, hotel: stanley)
+        king = Room.create!(suite: 'King', rate: 500, hotel: stanley)
+        poor = Room.create!(suite: 'Poor', rate: 50, hotel: grand_budapest)
+        matt = Guest.create!(name: 'Matt', nights: 3)
+        guestroom1 = GuestRoom.create!(guest: matt, room: pres )
+        guestroom2 = GuestRoom.create!(guest: matt, room: king )
 
         visit "/guests/#{matt.id}"
         
@@ -21,25 +21,28 @@ RSpec.describe 'the guest show page' do
         expect(page).to_not have_content("Poor")
       end
 
-      it "it has a form to add a room to this guest. When I fill in the fields and click submit I am redirected back to this guest show page and I see the room now listed under this guest" do
-        stanley = Hotel.create(name: "The Stanley", location: "Estes Park")
-        grand_budapest = Hotel.create(name: "The Grand Budapest Hotel", location: "Budapest")
-        pres = Room.create(suite: 'Presidential', rate: 400, hotel: stanley)
-        king = Room.create(suite: 'King', rate: 500, hotel: stanley)
-        poor = Room.create(suite: 'Poor', rate: 50, hotel: grand_budapest)
-        matt = Guest.create(name: 'Matt', nights: 3)
-        guestroom1 = GuestRoom.create(guest: matt, room: pres )
-        guestroom2 = GuestRoom.create(guest: matt, room: king )
+      it "it has a form to add a room to this guest. When I fill in the fields and click submit I am redirected back to this guest's show page and I see the room now listed under this guest" do
+        stanley = Hotel.create!(name: "The Stanley", location: "Estes Park")
+        grand_budapest = Hotel.create!(name: "The Grand Budapest Hotel", location: "Budapest")
+        pres = Room.create!(suite: 'Presidential', rate: 400, hotel: stanley)
+        king = Room.create!(suite: 'King', rate: 500, hotel: stanley)
+        poor = Room.create!(suite: 'Poor', rate: 50, hotel: grand_budapest)
+        matt = Guest.create!(name: 'Matt', nights: 3)
+        guestroom1 = GuestRoom.create!(guest: matt, room: pres )
+        guestroom2 = GuestRoom.create!(guest: matt, room: king )
 
         visit "/guests/#{matt.id}"
 
-        fill_in("Room ID", with: poor.id)
-        click_button("Submit")
-
-        expect(current_path).to eq("/guests/#{matt.id}")
         expect(page).to_not have_content("The Grand Budapest Hotel")
         expect(page).to_not have_content("Poor")
-        expect(page).to_not have_content("50")
+  
+        fill_in("Room ID", with: poor.id)
+        click_button("Submit")
+       save_and_open_page
+        expect(current_path).to eq("/guests/#{matt.id}")
+        expect(page).to have_content("The Grand Budapest Hotel")
+        expect(page).to have_content("Poor")
+        expect(page).to have_content("50")
       end
     end
   end
