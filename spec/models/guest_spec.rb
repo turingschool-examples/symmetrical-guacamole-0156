adba72684bbf3b503cd3e46ddccf1ee8b6b982b5
Jade Stewart <jadekstewart3@gmail.com>
2023-02-21 11:34:36 -0700

    Mess with scope, decide to leave as is

--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 RSpec.describe Guest, type: :model do
  it { should have_many :rooms }
 
- it '#count_guests' do 
+  describe ".scope" do 
+    it '.count_guests' do 
       Guest.create!(name: "Hannah", nights: 3)
       Guest.create!(name: "Phil", nights: 3)
       Guest.create!(name: "Denise", nights: 3)
 
       expect(Guest.count_guests).to eq(3)
     end
+  end
 end
