0e6caed538ce84d2ec1bace56010147930a795cc
KaraJH <karas.jones@yahoo.com>
2023-02-21 10:00:55 -0700

    test: Add room test, user story 2

--- spec/features/guests/show_spec.rb
+++ spec/features/guests/show_spec.rb
@@ -10,7 +10,8 @@ RSpec.describe "Guest Show Page" do
     @hotel_2 = Hotel.create!(name: "Subpar Hotel", location: "Denver")
 
     @room_1 = @hotel_1.rooms.create!(rate: 300, suite: "Standard")
-    @room_2 = @hotel_1.rooms.create!(rate: 650, suite: "Penthouse")
+    @room_2 = @hotel_1.rooms.create!(rate: 450, suite: "Penthouse")
+    @room_5 = @hotel_1.rooms.create!(rate: 500, suite: "Presidential")
 
     @room_3 = @hotel_2.rooms.create!(rate: 50, suite: "Standard")
     @room_4 = @hotel_2.rooms.create!(rate: 75, suite: "Presidential")
@@ -30,7 +31,7 @@ RSpec.describe "Guest Show Page" do
       
       expect(page).to have_content("Kimberly Kimbers")
       expect(page).to_not have_content(@guest_2.name)
-      
+
       within("##{@room_1.id}") do 
         expect(page).to have_content(@room_1.suite)
         expect(page).to have_content(@room_1.rate)
@@ -43,6 +44,25 @@ RSpec.describe "Guest Show Page" do
         expect(page).to have_content(@room_2.rate)
         expect(page).to have_content(@hotel_1.name)
         expect(page).to_not have_content(@room_4.rate)
+      end
+    end
+
+    it 'has a form to add an existing room to this guest and adds it to their show page' do 
+      visit "/guests/#{@guest_1.id}" 
+
+      within(".guest_rooms") do 
+        expect(page).to_not have_content(@room_5.suite)
+      end
+
+      within(".add_room_form") do 
+        fill_in("Room ID", with: @room_5.id)
+        click_button("Submit")
+
+        expect(current_path).to eq(visit "/guests/#{@guest_1.id}")
+      end
+
+      within(".guest_rooms") do 
+        expect(page).to have_content(@room_5.suite)
       end
     end
   end
