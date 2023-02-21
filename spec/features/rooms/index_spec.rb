670b4c58f6a56189e2a7413340e2c988b5bfd400
David Marino <davidjmarino8@gmail.com>
2023-02-21 14:02:58 -0500

    User Story 3 incomplete

--- spec/features/rooms/index_spec.rb
+++ spec/features/rooms/index_spec.rb
@@ -7,35 +7,35 @@ RSpec.describe 'index view' do
   let!(:room_2) { harbor.rooms.create!(rate: 185, suite: "Vice-Presidential")}
   let!(:room_3) { holliday.rooms.create!(rate: 160, suite: "Honeymoon")}
   let!(:room_4) { holliday.rooms.create!(rate: 10, suite: "Broom Closet")}
-  let!(:steve) { Guest.create!(name: 'Steve', nights: 3) }
-  let!(:dave) { Guest.create!(name: 'Dave', nights: 5) }
-  let!(:jim) { Guest.create!(name: 'Jim', nights: 1)}
-
-  before do
-    GuestRoom.create!(guest: steve, room: room_1)
-    GuestRoom.create!(guest: steve, room: room_2)
-    GuestRoom.create!(guest: dave, room: room_3)
-    GuestRoom.create!(guest: dave, room: room_4)
-  end
+  let!(:steve) { room_1.guests.create!(name: 'Steve', nights: 3) }
+  let!(:dave) { room_2.guests.create!(name: 'Dave', nights: 5) }
+  let!(:jim) { room_3.guests.create!(name: 'Jim', nights: 1)}
 
   it 'lists all rooms and associated attributes' do
     visit '/rooms'
 
+    GuestRoom.create!(guest: steve, room: room_1)
+    GuestRoom.create!(guest: dave, room: room_1)
+
+    GuestRoom.create!(guest: steve, room: room_2)
+
+    GuestRoom.create!(guest: dave, room: room_3)
+
+    GuestRoom.create!(guest: dave, room: room_4)
+
     expect(page).to have_content(room_1.suite)
     expect(page).to have_content(room_1.rate)
-    expect(page).to have_content(room_1.num_guests)
     expect(page).to have_content(harbor.name)
-    expect(page).to have_content(room_1.reservations)
+    expect(page).to have_content(room_1.guests.count)
 
+    
     expect(page).to have_content(room_2.suite)
     expect(page).to have_content(room_2.rate)
-    expect(page).to have_content(room_2.num_guests)
-    expect(page).to have_content(room_2.reservations)
+    expect(page).to have_content(room_2.guests.count)
 
     expect(page).to have_content(room_3.suite)
     expect(page).to have_content(room_3.rate)
-    expect(page).to have_content(room_3.num_guests)
     expect(page).to have_content(holliday.name)
-    expect(page).to have_content(room_3.reservations)
+    expect(page).to have_content(room_3.guests.count)
   end
 end
