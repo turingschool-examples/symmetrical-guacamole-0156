2e64f4cd279608f56998251f3093c79aa7b25728
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-02-21 10:50:53 -0700

    Add associated hotel to list - us1

--- spec/features/guests/show_spec.rb
+++ spec/features/guests/show_spec.rb
@@ -2,19 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "Guest Show Page" do
   # Story 1
-  # And I see a list of all the rooms they've stayed in
   # including the room's suite, nightly rate, and the name of the hotel that it belongs to.
 
   let!(:echo) { Hotel.create!(name: "Echo Mountain Inn", location: "Echo Mountain") }
-  # let!(:everest) { Hotel.create!(name: "Everest View Hotel", location: "Everest Mountain") }
+  let!(:everest) { Hotel.create!(name: "Everest View Hotel", location: "Everest Mountain") }
   let!(:charlize) { Guest.create!(name: "Charlize Theron", nights: 3) }
   let!(:scarlett) { Guest.create!(name: "Scarlett Johansson", nights: 2) }
   let!(:presidential) { echo.rooms.create!(rate: 125, suite: "Presidential") }
-  let!(:penthouse) { echo.rooms.create!(rate: 175, suite: "Penthouse") }
+  let!(:penthouse) { everest.rooms.create!(rate: 175, suite: "Penthouse") }
 
   before do
     GuestRoom.create!(guest: charlize, room: presidential)
-    GuestRoom.create!(guest: scarlett, room: penthouse)
+    GuestRoom.create!(guest: charlize, room: penthouse)
 
     visit "/guests/#{charlize.id}"
   end
@@ -28,8 +27,10 @@ RSpec.describe "Guest Show Page" do
       save_and_open_page
       expect(page).to have_content("Room: Presidential")
       expect(page).to have_content("Rate: 125")
-      expect(page).to_not have_content("Room: Penthouse")
-      expect(page).to_not have_content("Rate: 175")
+      expect(page).to have_content("Hotel Name: Echo Mountain Inn")
+      expect(page).to have_content("Room: Penthouse")
+      expect(page).to have_content("Rate: 175")
+      expect(page).to have_content("Hotel Name: Everest View Hotel")
     end
   end
 end
