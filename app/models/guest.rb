99f63126b0de96d5676ebf45b8a9270894cab72d
Melony Franchini <meltravelz@gmail.com>
2023-02-21 11:04:34 -0700

    Add model test & method for user story 3

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,4 +1,9 @@
 class Guest < ApplicationRecord
   has_many :guest_rooms
   has_many :rooms, through: :guest_rooms
+
+  def self.total_count
+    count(:id)
+  end
+
 end
--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe Guest, type: :model do
     it { should have_many :guest_rooms }
     it { should have_many(:rooms).through(:guest_rooms) }
   end
+
+  describe 'class methods' do
+    before do 
+      Guest.create!(name: "Letti")
+      Guest.create!(name: "Calum")
+      Guest.create!(name: "Nika")
+    end
+    
+    it "::total_count" do
+      expect(Guest.total_count).to eq(3)
+
+      Guest.create!(name: "Melony")
+
+      expect(Guest.total_count).to eq(4)
+    end
+  end
 end
