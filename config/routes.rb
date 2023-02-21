83cfc95db2f931c66024ee16bb52ffabc92d79e4
Adam Bailey <ab67319@gmail.com>
2023-02-21 11:20:29 -0700

    feat: create functionality for rooms index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/guests/:id', to: 'guests#show'
   post '/guests/:guest_id/rooms', to: 'room_guests#create'
+
+  get '/rooms', to: 'rooms#index'
 end
