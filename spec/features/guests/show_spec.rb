03a277230c30f0bffe283b5c93ab36944eadc053
Harrison Ryan <hwryan12@gmail.com>
2023-02-21 10:26:43 -0600

    feature: Add expectations to test for user story 1

--- spec/features/guests/show_spec.rb
+++ spec/features/guests/show_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Show", type: :feature do
   let!(:echo) { Hotel.create!(name: "Echo Mountain Inn", location: "Echo Mountain") }
   let!(:king) { echo.rooms.create!(rate: 125, suite: "King Bed Room") }
   let!(:twin) { echo.rooms.create!(rate: 75, suite: "Twin Bed Room") }
-  let!(:pres) { echo.rooms.create!(rate: 200, suite: "Presidential") }
+  let!(:pres) { echo.rooms.create!(rate: 200, suite: "Presidential Room") }
   let!(:jimmy) { echo.rooms.create!(name: "Jimmy", nights: 3) }
 
   before do
@@ -19,6 +19,14 @@ RSpec.describe "Show", type: :feature do
       it "I see the guest's name and I see a list of all the rooms they've stayed in
         including the room's suite, nightly rate, and the name of the hotel that it belongs to." do
 
+          expect(page).to have_content("Jimmy's Page")
+          expect(page).to have_content("King Bed Room")
+          expect(page).to have_content("$125.00")
+          expect(page).to have_content("Twin Bed Room")
+          expect(page).to have_content("$75.00")
+          expect(page).to have_content("Echo Mountain Inn")
+          expect(page).to_not have_content("Presidential Room")
+          expect(page).to_not have_content("$200.00")
       end
     end
   end
