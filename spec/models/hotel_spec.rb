07327014196b0461d042d1ee5afce912ff6c1ec7
Adam Bailey <ab67319@gmail.com>
2023-02-21 11:47:10 -0700

    test: create instance method test for #unique_guests

--- spec/models/hotel_spec.rb
+++ spec/models/hotel_spec.rb
@@ -9,4 +9,32 @@ RSpec.describe Hotel, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :location }
   end
+
+  describe '#instance methods' do
+    describe '#unique_guests' do
+      before(:each) do
+        @hotel = Hotel.create!(name: "The Hythe", location: "Vail, CO")
+
+        @junior_suite = Room.create!(suite: "Junior", rate: 250, hotel: @hotel)
+        @penthouse = Room.create!(suite: "Penthouse", rate: 1000, hotel: @hotel)
+        @standard_room = Room.create!(suite: "Standard Room", rate: 125, hotel: @hotel)
+
+        @adam = Guest.create!(name: "Adam", nights: 2)
+        @brie = Guest.create!(name: "Brie", nights: 5)
+        
+        RoomGuest.create!(guest: @adam, room: @junior_suite)
+        RoomGuest.create!(guest: @adam, room: @standard_room)
+        RoomGuest.create!(guest: @brie, room: @penthouse)
+      end
+      
+      it 'returns a list of unique guest names' do
+        expect(@hotel.unique_guests).to eq(["Adam", "Brie"])
+
+        josh = Guest.create!(name: "Josh", nights: 3)
+        RoomGuest.create!(guest: josh, room: @standard_room)
+
+        expect(@hotel.unique_guests).to eq(["Adam", "Brie", "Josh"])
+      end
+    end
+  end
 end
