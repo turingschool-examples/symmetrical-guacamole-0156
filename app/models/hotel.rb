cfa5a25ff767338576efc019c994df48eb22bac3
Bobby Luly <bobbyluly@gmail.com>
2023-02-21 11:57:41 -0700

    Got route, controller, and hotel show view created. Ran out of time trying to write the active record for class method

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,3 +1,7 @@
 class Hotel < ApplicationRecord
    has_many :rooms
+
+   def self.guest_names
+      joins(:rooms, :guests).where().pluck(:name)
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/hotels/:id', to: 'hotels#show'
+  
   get '/guests/:id', to: 'guests#show'
 
   get '/rooms', to: 'rooms#index'
