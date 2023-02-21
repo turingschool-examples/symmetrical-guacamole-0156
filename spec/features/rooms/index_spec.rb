f631f11c953b4f2bef9827a9a06e26eb7d818cfa
Brian Hayes <Brian.hayes1200@gmail.com>
2023-02-21 11:30:25 -0700

    Feat: add functionality to display total number of guests per room

--- spec/features/rooms/index_spec.rb
+++ spec/features/rooms/index_spec.rb
@@ -11,12 +11,28 @@ RSpec.describe 'Room Index' do
       RoomGuest.create!(room: room_2, guest: guest )
 
       visit '/rooms'
-      
+
       expect(page).to have_content(room_1.suite)
       expect(page).to have_content("Rate: #{room_1.rate}")
       expect(page).to have_content(room_2.suite)
       expect(page).to have_content("Rate: #{room_2.rate}")
       expect(page).to have_content("hotel: #{room_1.hotel.name}")
+    end
+
+    it 'can list the number of guests that have stayed in a room' do 
+      hotel = Hotel.create!(name: "Marriott", location: "Denver")
+      guest_1 = Guest.create!(name: "Brian", nights: 2)
+      guest_2 = Guest.create!(name: "Brian", nights: 2)
+      room_1 = Room.create!(rate: 100, suite: "President suite", hotel_id: hotel.id)
+      room_2 = Room.create!(rate: 150, suite: "Honeymoon Suite", hotel_id: hotel.id)
+      RoomGuest.create!(room: room_1, guest: guest_1)
+      RoomGuest.create!(room: room_2, guest: guest_2 )
+      RoomGuest.create!(room: room_2, guest: guest_1)
+
+      visit '/rooms'
+     
+      expect(page).to have_content("Number of total guests: #{room_1.guests.count}")
+      expect(page).to have_content("Number of total guests: #{room_2.guests.count}")
     end
   end
 end
