031ceac6a6b5f935de1f39ece63e19c4b41890d7
Adam Bailey <ab67319@gmail.com>
2023-02-21 11:47:27 -0700

    feat: create instance method #unique_guests

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -2,4 +2,8 @@ class Hotel < ApplicationRecord
   has_many :rooms
 
   validates_presence_of :name, :location
+
+  def unique_guests
+    Hotel.joins(rooms: :guests).where(id: self.id).distinct.pluck("guests.name")
+  end
 end
