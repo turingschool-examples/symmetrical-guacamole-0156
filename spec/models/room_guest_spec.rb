683579e4328c0c1a90839a43599652fc730be118
Adam Bailey <ab67319@gmail.com>
2023-02-21 09:19:11 -0700

    test: create model association tests

--- spec/models/room_guest_spec.rb
+++ spec/models/room_guest_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe RoomGuest, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to :room }
+  it { should belong_to :guest }
 end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Room, type: :model do
   describe 'relationships' do
-    it {should belong_to :hotel}
+    it { should belong_to :hotel }
+
+    it { should have_many :room_guests }
+    it { should have_many :guests }
   end
 end
