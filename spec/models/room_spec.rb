fbbe6af0a0a0215e946bda143f09a46a973fd063
Tori Enyart <torienyart@gmail.com>
2023-02-21 10:57:24 -0700

    Add guest counter for room instances

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -13,9 +13,17 @@ RSpec.describe Room, type: :model do
     let!(:tori) {Guest.create!(name: "Tori", nights_stayed: "7")}
     let!(:penthouse) {tori.rooms.create!(rate: 9998, suite: "Penthouse", hotel_id: mtn_1.id)}
     let!(:cabin) {tori.rooms.create!(rate: 8798, suite: "Private Cabin", hotel_id: mtn_2.id)}
+    let!(:suite) {tori.rooms.create!(rate: 998, suite: "Suite", hotel_id: mtn_1.id)}
+    let!(:joseph) {penthouse.guests.create!(name: "Joseph", nights_stayed: "7")}
 
     it 'can tell the hotel name of the room' do
       expect(cabin.hotel_name).to eq("Sun Valley Private")
+    end
+
+    it 'can tell the number of guests that have stayed' do
+      expect(penthouse.guests_stayed).to eq(2)
+      expect(suite.guests_stayed).to eq(1)
+      expect(cabin.guests_stayed).to eq(1)
     end
   end
 end
