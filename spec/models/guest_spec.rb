4649afbaed589347af17f8bc8a2fda512d92a57d
Sam Walker <sgwalker327@gmail.com>
2023-02-21 09:31:22 -0700

    Complete initial relationship & model setup

---
+++ spec/models/guest_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Guest, type: :model do
+  # desribe 'relationships' do
+  #   it {should have_many :rooms}
+  # end
+end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Room, type: :model do
   describe 'relationships' do
+    it {should have_many :guests}
     it {should belong_to :hotel}
   end
 end
