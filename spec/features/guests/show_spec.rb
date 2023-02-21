4b80890f5a5305799ba9745ca15e89ad61906de4
Melony Franchini <meltravelz@gmail.com>
2023-02-21 10:28:40 -0700

    Add feat test & view for user story 2

--- spec/features/guests/show_spec.rb
+++ spec/features/guests/show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe '/guests/:id', type: :feature do
-  describe "when I visit a guest show page I see" do
+  describe "when I visit a guest show page" do
     before do
       @hotel1 = Hotel.create!(name: "Ritz-Carlton", location: "Paris")
       @hotel2 = Hotel.create!(name: "St. Regis", location: "Geneva")
@@ -20,13 +20,27 @@ RSpec.describe '/guests/:id', type: :feature do
       visit "/guests/#{@guest1.id}"
     end
 
-    it "a list of all the rooms they've stayed in, rate, suite, & hotel it belongs to" do
+    it "I see a list of all the rooms they've stayed in, rate, suite, & hotel it belongs to" do
       expect(page).to have_content("#{@guest1.name}'s Page")
       expect(page).to have_content("Past Stays:")
       expect(page).to have_content("#{@room1.suite} - rate: $#{@room1.rate}/night, hotel: #{@hotel1.name}")
       expect(page).to have_content("#{@room2.suite} - rate: $#{@room2.rate}/night, hotel: #{@hotel1.name}")
       expect(page).to have_content("#{@room3.suite} - rate: $#{@room3.rate}/night, hotel: #{@hotel2.name}")
       expect(page).to_not have_content("#{@room4.suite}")
+    end
+
+    it "I see a form to add a room to this guest" do
+      expect(page).to have_content("Book a New Room:")
+      expect(page).to have_field(:room_id)
+      expect(page).to have_button("Submit")
+    end
+
+    it "I fill in the form with existing ID of room, click submit, redirected back & I see the new room" do
+      fill_in("Book a New Room:", with: "#{@room4.id}")
+      click_button("Submit")
+
+      expect(current_path).to eq("/guests/#{@guest1.id}")
+      expect(page).to have_content("#{@room4.suite} - rate: $#{@room4.rate}/night, hotel: #{@hotel2.name}")
     end
 
   end
