a679fc719436169c177394b199a776aa088e5682
Bobby Luly <bobbyluly@gmail.com>
2023-02-21 10:36:25 -0700

    Added seeds to test functionality of adding a room to a guest

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  hotel_1 = Hotel.create!(name: 'The Stanley', location: 'Estes Park')
+  room_1 = hotel_1.rooms.create!(rate: 50, suite: 'Suite 1')
+  room_2 = hotel_1.rooms.create!(rate: 60, suite: 'Suite 2')
+  john = Guest.create!(name: 'John Smith', nights: 2)
+
+  GuestRoom.create!(guest: john, room: room_1)
+  GuestRoom.create!(guest: john, room: room_2)
+  
+  room_3 = hotel_1.rooms.create!(rate: 55, suite: 'Suite 3')
