949361ca4f8401d6abb1e63317c271a381a87aa5
Weston Sandfort <sandfortw@gmail.com>
2023-02-21 09:26:37 -0700

    add basic model tests for room and hotel

--- spec/models/hotel_spec.rb
+++ spec/models/hotel_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe Hotel, type: :model do
   describe 'relationships' do
     it {should have_many :rooms}
   end
+
+  it '#name' do
+    budapest = Hotel.create!(name: "Grand Budapest", location: 'Turkey')
+    expect(budapest.name).to eq("Grand Budapest")
+  end
+
+  it '#location' do
+    budapest = Hotel.create!(name: "Grand Budapest", location: 'Turkey')
+    expect(budapest.location).to eq("Turkey")
+  end
 end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe Room, type: :model do
     it {should have_many :guest_rooms}
     it {should have_many(:guests).through(:guest_rooms)}
   end
+
+  it '#suite' do
+    budapest = Hotel.create!(name: "Grand Budapest", location: 'Turkey')
+    room1 = Room.create!(hotel_id: budapest.id, suite: "Presidential", rate: 125)
+    expect(room1.suite).to eq("Presidential")
+  end
+
+  it '#rate' do
+    budapest = Hotel.create!(name: "Grand Budapest", location: 'Turkey')
+    room1 = Room.create!(hotel_id: budapest.id, suite: "Presidential", rate: 125)
+    expect(room1.rate).to eq(125)
+  end
 end
