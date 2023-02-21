e492488c0c51370102c992e31e2ea21400c7c260
Adam Bailey <ab67319@gmail.com>
2023-02-21 11:17:36 -0700

    refactor: remove ::total_guests from rooms model

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -13,23 +13,4 @@ RSpec.describe Room, type: :model do
     it { should validate_presence_of :rate }
     it { should validate_numericality_of :rate }
   end
-
-  describe '::Class Methods' do
-    describe '::total_guests' do
-      it "returns the total number of guests stayed in room" do
-        hythe = Hotel.create!(name: "The Hythe", location: "Vail, CO")
-        junior_suite = Room.create!(suite: "Junior", rate: 250, hotel: hythe)
-        adam = Guest.create!(name: "Adam", nights: 2)
-        brie = Guest.create!(name: "Brie", nights: 5)
-
-        RoomGuest.create!(guest: adam, room: junior_suite)
-  
-        expect(junior_suite.total_guests).to eq(1)
-
-        RoomGuest.create!(guest: brie, room: junior_suite)
-
-        expect(junior_suite.total_guests).to eq(2)
-      end
-    end
-  end
 end
