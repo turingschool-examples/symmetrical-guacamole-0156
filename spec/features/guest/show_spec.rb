c610b9e55938e4fe87694a0a2d657a9ace05f206
Chris Crane <chrisjcrane@gmail.com>
2023-02-21 09:38:47 -0700

    add template for user story 1 tests

---
+++ spec/features/guest/show_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe "Guest#Show" do
+  before(:each) do
+
+  end
+
+  it "shows the guests NAMES" do
+    
+  end
+
+  it "shows a list of all the ROOMS they've stayed in" do
+    
+  end
+
+  it "includes the rooms SUITE, nightly RATE, and HOTEL NAME it belongs to" do
+    
+  end
+end
