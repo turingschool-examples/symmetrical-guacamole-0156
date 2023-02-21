f04ff963e2b081b96c78b548251971b4f2715ce7
Bobby Luly <bobbyluly@gmail.com>
2023-02-21 11:25:16 -0700

    Feat: adds all functionality to complete user story 3 and it's tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/guests/:id', to: 'guests#show'
 
+  get '/rooms', to: 'rooms#index'
+
   post 'guests/:id/guest_rooms', to: 'guest_rooms#create'
 end
