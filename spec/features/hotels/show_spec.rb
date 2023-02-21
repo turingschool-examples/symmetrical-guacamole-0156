d124214e765a48519136aad1f3dc6c74bb227e4e
Hady Matar <hadyematar@gmail.com>
2023-02-21 10:39:35 -0700

    Add model test for User Story 3

---
+++ spec/features/hotels/show_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe 'Hotels show feature spec' do
+before(:each) do 
+  @hotel_1 = Hotel.create!(name: "Losodeli", location: "Rinconada")
+  @hotel_2 = Hotel.create!(name: "Casa Flow", location: "Zicatela")
+
+  @room_san_jose = @hotel_1.rooms.create!(rate: 80, suite: "Presidential")
+  @room_oaxaca = @hotel_1.rooms.create!(rate: 100, suite: "Twin")
+  @room_jocotopec = @hotel_1.rooms.create!(rate: 50, suite: "Pauper")
+  @room_da_flow = @hotel_2.rooms.create!(rate:300, suite: "Beachside")
+
+  @guest_hady = Guest.create!(name: "Hady", nights: 12)
+  @guest_malena = Guest.create!(name: "Malena", nights: 6)
+  @guest_sebastian = Guest.create!(name: "Sebastian", nights: 8)
+
+  GuestRoom.create!(guest_id: @guest_hady.id, room_id: @room_oaxaca.id)
+  GuestRoom.create!(guest_id: @guest_hady.id, room_id: @room_jocotopec.id)
+  GuestRoom.create!(guest_id: @guest_hady.id, room_id: @room_da_flow.id)
+
+  GuestRoom.create!(guest_id: @guest_malena.id, room_id: @room_jocotopec.id)
+  GuestRoom.create!(guest_id: @guest_malena.id, room_id: @room_da_flow.id)
+
+  GuestRoom.create!(guest_id: @guest_sebastian.id, room_id: @room_da_flow.id)
+  GuestRoom.create!(guest_id: @guest_sebastian.id, room_id: @room_san_jose.id)
+
+end 
+
+  describe "as a visitor" do
+    describe "when visit the hotel show page" do 
+      it "displays a unique list of all the guests at the hotel" do
+
+        visit "/hotels/#{@hotel_1.id}"
+
+        expect
+
+
+
+
+
+
+      end 
+    end 
+  end 
+end 
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -6,4 +6,34 @@ RSpec.describe Room, type: :model do
     it {should have_many :guest_rooms}
     it {should have_many(:guests).through(:guest_rooms)}
   end
+
+  describe "instance methods" do 
+    before(:each) do 
+      @hotel_1 = Hotel.create!(name: "Losodeli", location: "Rinconada")
+      @hotel_2 = Hotel.create!(name: "Casa Flow", location: "Zicatela")
+    
+      @room_san_jose = @hotel_1.rooms.create!(rate: 80, suite: "Presidential")
+      @room_oaxaca = @hotel_1.rooms.create!(rate: 100, suite: "Twin")
+      @room_jocotopec = @hotel_1.rooms.create!(rate: 50, suite: "Pauper")
+      @room_da_flow = @hotel_2.rooms.create!(rate:300, suite: "Beachside")
+    
+      @guest_hady = Guest.create!(name: "Hady", nights: 12)
+      @guest_malena = Guest.create!(name: "Malena", nights: 6)
+      @guest_sebastian = Guest.create!(name: "Sebastian", nights: 8)
+    
+      GuestRoom.create!(guest_id: @guest_hady.id, room_id: @room_oaxaca.id)
+      GuestRoom.create!(guest_id: @guest_hady.id, room_id: @room_jocotopec.id)
+      GuestRoom.create!(guest_id: @guest_hady.id, room_id: @room_da_flow.id)
+    
+      GuestRoom.create!(guest_id: @guest_malena.id, room_id: @room_jocotopec.id)
+      GuestRoom.create!(guest_id: @guest_malena.id, room_id: @room_da_flow.id)
+    
+      GuestRoom.create!(guest_id: @guest_sebastian.id, room_id: @room_da_flow.id)
+      GuestRoom.create!(guest_id: @guest_sebastian.id, room_id: @room_san_jose.id)
+    
+    end 
+    it "#count_guests" do 
+      expect(@room_da_flow.count_guests).to eq(3)
+    end
+  end
 end
