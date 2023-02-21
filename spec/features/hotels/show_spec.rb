54d9c4ac6f1d610775b6d738625e5b82d8626932
Brian Hayes <Brian.hayes1200@gmail.com>
2023-02-21 11:37:30 -0700

    Test: add test for hotel show page displaying unique list of guests

---
+++ spec/features/hotels/show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Hotel Show' do 
+  describe 'as a visitor' do 
+    describe 'when I visit a Hotel show page' do 
+      it 'will have a unique list of guests named who have stayed at this hotel' do 
+        hotel = Hotel.create!(name: "Marriott", location: "Denver")
+        guest_1 = Guest.create!(name: "Brian", nights: 2)
+        guest_2 = Guest.create!(name: "John", nights: 2)
+        room_1 = Room.create!(rate: 100, suite: "President suite", hotel_id: hotel.id)
+        room_2 = Room.create!(rate: 150, suite: "Honeymoon Suite", hotel_id: hotel.id)
+        RoomGuest.create!(room: room_1, guest: guest_1)
+        RoomGuest.create!(room: room_2, guest: guest_2 )
+        RoomGuest.create!(room: room_2, guest: guest_1)
+  
+
+        visit "/hotels/#{hotel.id}"
+
+        expect(page).to have_content(guest_1.name)
+        expect(page).to have_content(guest_2.name)
+
+      end 
+    end 
+  end 
+end 
