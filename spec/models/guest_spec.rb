9cad630470e3c2d51aa63e499cfe7ed19d889a27
Adam Bailey <ab67319@gmail.com>
2023-02-21 11:19:25 -0700

    test: create class method test for ::total_guests

--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -11,4 +11,18 @@ RSpec.describe Guest, type: :model do
     it { should validate_presence_of :nights }
     it { should validate_numericality_of :nights }
   end
+  
+  describe '::Class Methods' do
+    describe '::total_guests' do
+      it "returns the total number of guests" do
+        Guest.create!(name: "Adam", nights: 2)
+
+        expect(Guest.total_guests).to eq(1)
+        
+        Guest.create!(name: "Brie", nights: 5)
+
+        expect(Guest.total_guests).to eq(2)
+      end
+    end
+  end
 end
