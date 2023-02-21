6e313d2f3ff26578fd4e1920cc70b7a6ce4b33bb
Isaac Alter <i_alter@yahoo.com>
2023-02-21 11:24:40 -0700

    Adds feat: User Story 3 COMPLETE

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,4 +1,8 @@
 class Guest < ApplicationRecord
   has_many :guest_rooms
   has_many :rooms, through: :guest_rooms
+
+  def self.count_guests
+    count(:id)
+  end
 end
--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe Guest, type: :model do
     it{should have_many :guest_rooms}
     it{should have_many :rooms}
   end
+
+  describe "class methods" do
+    it "#count" do
+      guest1 = Guest.create!(name: "Bob", nights: 3)
+      guest2 = Guest.create!(name: "Shane", nights: 10)
+
+      expect(Guest.count_guests).to eq(2)
+
+      guest3 = Guest.create!(name: "Isaac", nights: 13)
+
+      expect(Guest.count_guests).to eq(3)
+    end
+  end
 end
