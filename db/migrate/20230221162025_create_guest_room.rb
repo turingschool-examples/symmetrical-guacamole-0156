898d21f6cc6ab6be151df8040a62de5146e806cd
KaraJH <karas.jones@yahoo.com>
2023-02-21 09:23:34 -0700

    feat: Migrations for guests and guest rooms

---
+++ db/migrate/20230221162025_create_guest_room.rb
@@ -0,0 +1,10 @@
+class CreateGuestRoom < ActiveRecord::Migration[5.2]
+  def change
+    create_table :guest_rooms do |t|
+      t.references :room, foreign_key: true
+      t.references :guest, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
