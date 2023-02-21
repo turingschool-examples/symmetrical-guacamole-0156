c3e373d26a584e9d5a1a4622438daa2373879170
Caleb Thomas <cjthomas00@comcast.net>
2023-02-21 09:18:57 -0700

    create guests, guest_rooms tables and all relationships.

---
+++ spec/models/guest_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Guest, type: :model do
+  describe "relationships" do
+    it { should have_many(:guest_rooms) }
+    it { should have_many(:rooms).through(:guest_rooms) }
+  end
+end
