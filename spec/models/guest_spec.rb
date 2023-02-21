101ce28a2d31657bfca4fa0bcb43ed2e6fa3a51c
Matt Enyeart <menyeart1@gmail.com>
2023-02-21 10:52:49 -0700

    Add count guests test for guest model spec

--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe Guest, type: :model do
     it {should have_many :guest_rooms}
     it {should have_many(:rooms).through(:guest_rooms)}
   end
+
+  describe 'class methods' do
+    it "can count the total number of guests" do
+      matt = Guest.create!(name: 'Matt', nights: 5)
+      keith = Guest.create!(name: 'Keith', nights: 2)
+      stephanie = Guest.create!(name: 'Stephanie', nights: 4)
+
+      expect(Guest.count_guests).to eq(3)
+
+      sunita = Guest.create!(name: 'Sunita', nights: 4)
+
+      expect(Guest.count_guests).to eq(4)
+    end
+  end
 end
