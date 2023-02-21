c2e48a5a807972dfe83b8f79b9c14e4c7dabeea3
Meredith Trevino <meredithtrevino@gmail.com>
2023-02-21 10:23:30 -0700

    working user story 2

--- app/controllers/room_guests_controller.rb
+++ app/controllers/room_guests_controller.rb
@@ -1,7 +1,9 @@
 class RoomGuestsController < ApplicationController
 
   def create
-    RoomGuest.new(room_id: params[:room_id], guest_id: params[:id])
+    room_guest = RoomGuest.new(room_id: params[:room_id], guest_id: params[:id])
+    
+    room_guest.save
     redirect_to "/guests/#{params[:id]}"
   end
 end
