b1e6a69ca042c9a72f9665deab8986b7847a6efd
Weston Sandfort <sandfortw@gmail.com>
2023-02-21 09:29:58 -0700

    add basic tests for guest

--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Guest, type: :model do
     it {should have_many :guest_rooms}
     it {should have_many(:rooms).through(:guest_rooms)}
   end
+
+  it '#name' do
+    charlize = Guest.create!(name:"Charlize Theron", nights: 3)
+    expect(charlize.name).to eq("Charlize Theron")
+  end
+
+  it '#nights' do
+    charlize = Guest.create!(name:"Charlize Theron", nights: 3)
+    expect(charlize.nights).to eq(3)
+  end
 end
