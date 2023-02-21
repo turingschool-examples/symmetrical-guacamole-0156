33c90043264d4a1389343dc9a5ff7b66f7cc5e32
Jade Stewart <jadekstewart3@gmail.com>
2023-02-21 10:31:20 -0700

    Create route to create action in GuestRooms controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/guests/:id', to: 'guests#show'
+
+  post '/guests/:id/guest_rooms', to: 'guest_rooms#create'
 end
