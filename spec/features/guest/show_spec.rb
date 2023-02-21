ce66953a329e26184641bde32fe30a0c349d62e5
Isaac Alter <i_alter@yahoo.com>
2023-02-21 10:25:18 -0700

    Adds feat: User Story 1 COMPLETE

--- spec/features/guest/show_spec.rb
+++ spec/features/guest/show_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 RSpec.describe "guest show page" do
   before do
     @hotel1 = Hotel.create!(name: "Holiday Inn", location: "Las Vegas")
+    @hotel2 = Hotel.create!(name: "Mariott", location: "Austin")
+    @hotel3 = Hotel.create!(name: "Condor", location: "Denver")
 
     @room1 = @hotel1.rooms.create!(rate: 150, suite: "Economy")
-    @room2 = @hotel1.rooms.create!(rate: 275, suite: "Double")
+    @room2 = @hotel2.rooms.create!(rate: 275, suite: "Double")
     @room3 = @hotel1.rooms.create!(rate: 350, suite: "First Class")
-    @room4 = @hotel1.rooms.create!(rate: 575, suite: "Penthouse")
+    @room4 = @hotel3.rooms.create!(rate: 575, suite: "Penthouse")
 
     @guest1 = @room1.guests.create!(name: "Bob", nights: 3)
     @guest2 = @room1.guests.create!(name: "Shane", nights: 10)
@@ -19,19 +21,18 @@ RSpec.describe "guest show page" do
     visit guest_path(@guest1)
   end
 
-  scenario "When I visit guest''s  show page" do
+  scenario "When I visit guest's  show page" do
     expect(current_path).to eq("/guests/#{@guest1.id}")
   end
 
-  scenario "I see the guest''s name" do
+  scenario "I see the guest's name" do
     expect(page).to have_content("Bob")
     expect(page).to have_content(@guest1.name)
     expect(page).to_not have_content("Shane")
     expect(page).to_not have_content(@guest2.name)
   end
 
-  scenario "I see a list of all the rooms they''ve stayed in" do
-    save_and_open_page
+  scenario "I see a list of all the rooms they've stayed in" do
     expect(page).to have_content("Economy")
     expect(page).to have_content("Double")
     expect(page).to have_content("First Class")
@@ -41,5 +42,25 @@ RSpec.describe "guest show page" do
 
     expect(page).to_not have_content("Penthouse")
     expect(page).to_not have_content(@room4.suite)
+  end
+
+  scenario "I see a said rooms' nightly rates and the name of the hotel they belong to." do
+    save_and_open_page
+    expect(page).to have_content(150)
+    expect(page).to have_content(275)
+    expect(page).to have_content(350)
+    expect(page).to have_content(@room1.rate)
+    expect(page).to have_content(@room2.rate)
+    expect(page).to have_content(@room3.rate)
+
+    expect(page).to have_content("Holiday Inn")
+    expect(page).to have_content("Mariott")
+    expect(page).to have_content(@hotel1.name)
+    expect(page).to have_content(@hotel2.name)
+
+    expect(page).to_not have_content("575")
+    expect(page).to_not have_content(@room4.rate)
+    expect(page).to_not have_content("Condor")
+    expect(page).to_not have_content(@hotel3.name)
   end
 end
