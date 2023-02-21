5ce0bfdad8fbad4dbea2e641317416bb8506e038
Brian Hayes <Brian.hayes1200@gmail.com>
2023-02-21 10:24:06 -0700

    Test: add test for show page having form to add new room

--- spec/features/guests/show_spec.rb
+++ spec/features/guests/show_spec.rb
@@ -25,6 +25,50 @@ RSpec.describe 'Guest Show' do
        
         }
       end
+
+      it 'will have render a form to add a room to this guest' do 
+        hotel = Hotel.create!(name: "Marriott", location: "Denver")
+        guest = Guest.create!(name: "Brian", nights: 2)
+        room_1 = Room.create!(rate: 100, suite: "President suite", hotel_id: hotel.id)
+        room_2 = Room.create!(rate: 150, suite: "Honeymoon Suite", hotel_id: hotel.id)
+        room_3 = Room.create!(rate: 1000, suite: "Space Suite", hotel_id: hotel.id)
+        RoomGuest.create!(room: room_1, guest: guest )
+        RoomGuest.create!(room: room_2, guest: guest )
+
+        visit "/guests/#{guest.id}"
+        
+        within('.form') {
+        expect(page).to have_content('Add a New Room')
+        expect(page).to have_field(:room_id)
+        }
+      end
+
+      it 'will have the ability to fill form out and add a new room' do 
+        hotel = Hotel.create!(name: "Marriott", location: "Denver")
+        guest = Guest.create!(name: "Brian", nights: 2)
+        room_1 = Room.create!(rate: 100, suite: "President suite", hotel_id: hotel.id)
+        room_2 = Room.create!(rate: 150, suite: "Honeymoon Suite", hotel_id: hotel.id)
+        room_3 = Room.create!(rate: 1000, suite: "Space Suite", hotel_id: hotel.id)
+        RoomGuest.create!(room: room_1, guest: guest )
+        RoomGuest.create!(room: room_2, guest: guest )
+
+        visit "/guests/#{guest.id}"
+
+        within('.rooms') {
+          expect(page).to have_no_content(room_3.suite)
+          expect(page).to have_no_content(room_3.rate)
+          expect(page).to have_no_content(room_3.hotel.name)
+        }
+
+        fill_in :room_id, with: room_3.id
+        click_button 'submit'
+
+        within('.rooms') {
+          expect(page).to have_content(room_3.suite)
+          expect(page).to have_content(room_3.rate)
+          expect(page).to have_content(room_3.hotel.name)
+        }
+      end 
     end
   end
 end
