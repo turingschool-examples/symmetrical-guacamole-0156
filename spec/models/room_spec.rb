03d6b40e1eac97d756c9cccb87fc469c9978b16f
Conner Van Loan <cdvanloan@gmail.com>
2023-02-21 10:06:51 -0700

    Adds model test for #hotel_name

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe Room, type: :model do
   describe 'relationships' do
     it {should belong_to :hotel}
   end
+
+  describe '#hotel_name' do
+    it 'can return the name of the hotel it belongs to' do
+      @red_cliffs = Hotel.create!(name: "Red Cliffs Lodge", location: "Moab")
+      @river_side = @red_cliffs.rooms.create!(rate: 250, suite: "River Side")
+
+      expect(@river_side.hotel_name).to eq "Red Cliffs Lodge"
+    end
+  end
 end
