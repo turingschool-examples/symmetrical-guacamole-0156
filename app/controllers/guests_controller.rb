62ac5182a6f3a868fe375ee9ea225706070b7be2
Isaac Alter <i_alter@yahoo.com>
2023-02-21 10:11:13 -0700

    Adds feat: testing and logic for third line User Story 1

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -2,5 +2,6 @@ class GuestsController < ApplicationController
 
   def show
     @guest = Guest.find(params[:id])
+    @rooms = @guest.rooms
   end
 end
--- spec/features/guest/show_spec.rb
+++ spec/features/guest/show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'guest show page' do
+RSpec.describe "guest show page" do
   before do
     @hotel1 = Hotel.create!(name: "Holiday Inn", location: "Las Vegas")
 
@@ -19,15 +19,27 @@ RSpec.describe 'guest show page' do
     visit guest_path(@guest1)
   end
 
-  scenario 'When I visit guest''s  show page' do
+  scenario "When I visit guest''s  show page" do
     expect(current_path).to eq("/guests/#{@guest1.id}")
   end
 
-  scenario 'I see the guest''s name' do
-    save_and_open_page
+  scenario "I see the guest''s name" do
     expect(page).to have_content("Bob")
     expect(page).to have_content(@guest1.name)
     expect(page).to_not have_content("Shane")
     expect(page).to_not have_content(@guest2.name)
+  end
+
+  scenario "I see a list of all the rooms they''ve stayed in" do
+    save_and_open_page
+    expect(page).to have_content("Economy")
+    expect(page).to have_content("Double")
+    expect(page).to have_content("First Class")
+    expect(page).to have_content(@room1.suite)
+    expect(page).to have_content(@room2.suite)
+    expect(page).to have_content(@room3.suite)
+
+    expect(page).to_not have_content("Penthouse")
+    expect(page).to_not have_content(@room4.suite)
   end
 end
