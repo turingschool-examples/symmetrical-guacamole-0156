199be1feddc91f95c3ec24f6624ee2320e71c9d4
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-02-21 20:05:55 -0700

    Add formatting changes

--- app/controllers/guest_rooms_controller.rb
+++ app/controllers/guest_rooms_controller.rb
@@ -1,11 +1,8 @@
 class GuestRoomsController < ApplicationController
   def create
-    # gr = GuestRoom.create(guest_id: guest, room_id: room)
     gr = GuestRoom.new(room_id: params[:room_id], guest_id: params[:id])
-    # require 'pry'; binding.pry
-    # why new instead of create
+    # why new instead of create?
     gr.save
     redirect_to "/guests/#{params[:id]}"
-
   end
 end
